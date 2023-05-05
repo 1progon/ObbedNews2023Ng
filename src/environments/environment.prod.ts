daa007999c62f4b99aa91b74b364bb17999ee455
Anton V <gageviolo@gmail.com>
2023-05-05 13:09:14 +0530

    Update name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   production: true,
   domain: domain,
   websiteName: 'Оббед',
+  websiteNameLong: 'Английский на Оббед',
   apiBase: domain + '/api',
   imagesPath: '/images',
   videoServer: 'https://girlsimages.com/videos/news',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   domain: 'https://localhost:4200',
   websiteName: '[Dev] Obbed',
+  websiteNameLong: '[Dev] Английский на Obbed',
   apiBase: 'https://localhost:5001/api',
   imagesPath: 'http://localhost:8000/images',
   videoServer: 'https://girlsimages.com/videos/news',
